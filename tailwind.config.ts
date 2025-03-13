36432b6b69b4bc17154e874550f55ec08d5ba1b5
Kinshuk Ghildial <dev.kghildial@gmail.com>
2025-03-13 19:29:18 +0530

    feat(infra): add Prettier configuration and update Tailwind CSS with project styles & fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,62 +1,91 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
-    darkMode: ["class"],
-    content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  darkMode: ['class'],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-  	extend: {
-  		colors: {
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    extend: {
+      fontFamily: {
+        heading: 'var(--font-bebas_neue)',
+        title: 'var(--font-inknut_antiqua)',
+        body: 'var(--font-poppins)',
+        ui: 'var(--font-poppins)',
+      },
+      fontSize: {
+        // Mobile & Tablet Sizes
+        h1: ['2.25rem', { lineHeight: '2.625rem' }], // 36px | 42px
+        h2: ['1.875rem', { lineHeight: '2.25rem' }], // 30px | 36px
+        h3: ['1.5rem', { lineHeight: '1.875rem' }], // 24px | 30px
+        h4: ['1.25rem', { lineHeight: '1.625rem' }], // 20px | 26px
+        h5: ['1.125rem', { lineHeight: '1.5rem' }], // 18px | 24px
+        h6: ['1rem', { lineHeight: '1.375rem' }], // 16px | 22px
+        // Desktop Sizes
+        'h1-lg': ['4rem', { lineHeight: '4.375rem' }], // 64px | 70px
+        'h2-lg': ['3rem', { lineHeight: '3.375rem' }], // 48px | 54px
+        'h3-lg': ['2.25rem', { lineHeight: '2.625rem' }], // 36px | 42px
+        'h4-lg': ['1.75rem', { lineHeight: '2.125rem' }], // 28px | 34px
+        'h5-lg': ['1.5rem', { lineHeight: '1.875rem' }], // 24px | 30px
+        'h6-lg': ['1.25rem', { lineHeight: '1.625rem' }], // 20px | 26px
+        // Common Sizes
+        body: ['1rem', { lineHeight: '1.5rem' }], // 16px | 24px
+      },
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        border: 'var(--border)',
+        borderRadius: {
+          lg: 'var(--radius)',
+          md: 'calc(var(--radius) - 4px)',
+          sm: 'calc(var(--radius) - 6px)',
+          xs: 'calc(var(--radius) - 8px)',
+        },
+        // ShadCN config untouched
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        chart: {
+          '1': 'hsl(var(--chart-1))',
+          '2': 'hsl(var(--chart-2))',
+          '3': 'hsl(var(--chart-3))',
+          '4': 'hsl(var(--chart-4))',
+          '5': 'hsl(var(--chart-5))',
+        },
+      },
+      screens: {
+        md: '768px',
+      },
+    },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require('tailwindcss-animate')],
 } satisfies Config;
