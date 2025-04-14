5037cb40ed8bac9c96cbbb08ba4ff8915b3a25a8
Kinshuk Ghildial <dev.kghildial@gmail.com>
2025-04-14 12:26:37 +0530

    feat(ui): enhance MangaCard with hover effects and improve layout

--- src/widgets/MangaCard/Compact/index.tsx
+++ src/widgets/MangaCard/Compact/index.tsx
@@ -16,13 +16,19 @@ const Compact = React.forwardRef<HTMLDivElement, IMangaCard>(
     }
 
     return (
-      <Card ref={ref} className={cn('flex p-2.5', className)}>
+      <Card
+        ref={ref}
+        className={cn(
+          'group flex p-2.5 transition-all hover:bg-accent_tint hover:text-background',
+          className,
+        )}
+      >
         <Image
           src={`https://uploads.mangadex.org/covers/${id}/${coverArtFileName}.256.jpg`}
           alt={title ?? 'No Title'}
           width={60}
           height={80}
-          className="h-[80px] rounded-xs border border-foreground"
+          className="h-[80px] rounded-xs border border-foreground transition-all group-hover:border-background"
         />
         <div className="ml-2.5 flex flex-1 flex-col justify-between">
           <CardHeader className="">
@@ -30,8 +36,12 @@ const Compact = React.forwardRef<HTMLDivElement, IMangaCard>(
             {description && <CardDescription>{description}</CardDescription>}
           </CardHeader>
           <CardFooter className="flex justify-between font-ui">
-            <p className="font-3.25 text-xs/4 font-medium">Chapter {chapter}</p>
-            <p className="font-3.25 text-[0.625rem]/4 font-medium">{timestamp}</p>
+            <p className="font-3.25 text-xs/4 font-medium group-hover:text-background">
+              Chapter {chapter}
+            </p>
+            <p className="font-3.25 text-[0.625rem]/4 font-medium group-hover:text-background">
+              {timestamp}
+            </p>
           </CardFooter>
         </div>
       </Card>
