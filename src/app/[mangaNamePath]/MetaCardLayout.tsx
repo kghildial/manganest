9c3e827e3f3d9b457b0c8958ef2affef65db50d7
Kinshuk Ghildial <dev.kghildial@gmail.com>
2025-04-16 17:45:27 +0530

    feat(ui): add rating + responsive ui to manga deails page

--- src/app/[mangaNamePath]/MetaCardLayout.tsx
+++ src/app/[mangaNamePath]/MetaCardLayout.tsx
@@ -7,7 +7,7 @@ import { cn } from '@/lib/utils';
 import { IMetaCardLayout } from './MangaDetails.types';
 
 const MetaCardLayout: ReactFC<IMetaCardLayout> = ({ title, children, className }) => (
-  <Card className={cn('max-w-max px-3 py-1 md:max-w-[30%] md:px-5 md:py-2', className)}>
+  <Card className={cn('max-w-max px-3 pb-2 pt-1 md:max-w-[30%] md:px-5 md:py-2', className)}>
     <CardTitle className="mb-1 font-title text-base md:text-xl/6">{title}</CardTitle>
     <CardContent className="flex flex-wrap gap-1 text-xs md:text-base">{children}</CardContent>
   </Card>
--- src/lib/manga.server.ts
+++ src/lib/manga.server.ts
@@ -4,7 +4,7 @@
  * Server utils for manga
  */
 
-import { IGetMangaParams, IGetMangaResponse } from '@/types/manga.types';
+import { IGetMangaParams, IGetMangaResponse, IGetMangaStatsResponse } from '@/types/manga.types';
 import { createMangaQueryParams } from './manga';
 import { notFound } from 'next/navigation';
 
@@ -33,5 +33,27 @@ export async function getManga(params: IGetMangaParams): Promise<IGetMangaRespon
   } catch (error) {
     console.error('Error fetching manga:', error);
     notFound();
+  }
+}
+
+export async function getMangaStats(id: string): Promise<IGetMangaStatsResponse> {
+  if (!process.env.MANGADEX_BASE_API_URL) {
+    throw new Error('MANGADEX_BASE_API_URL is not defined in the environment variables.');
+  }
+
+  try {
+    const url = `${process.env.MANGADEX_BASE_API_URL}/statistics/manga/${id}`;
+
+    const response = await fetch(url, { cache: 'force-cache' });
+
+    if (!response.ok) {
+      console.error(`Failed to fetch manga stats: ${response.status} ${response.statusText}`);
+      notFound();
+    }
+
+    return response.json();
+  } catch (err) {
+    console.error('Error fetching manga:', err);
+    throw new Error('Failed to fetch manga stats!');
   }
 }
