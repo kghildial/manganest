f7df00727b67bb7a4390dce3b7a9299379d4f4cc
Kinshuk Ghildial <dev.kghildial@gmail.com>
2025-04-23 14:05:28 +0530

    feat(manga_reader): add failsafes for empty manga feed results

---
+++ src/app/404/page.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="flex h-[80vh] w-full flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">404 - Page Not Found</h1>
+      <p className="mt-4 font-body">The page you’re looking for doesn’t exist.</p>
+    </div>
+  );
+}
