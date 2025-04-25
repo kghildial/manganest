a597b94e0f729e60b14165a62716b9057d3a5078
Kinshuk Ghildial <dev.kghildial@gmail.com>
2025-04-25 23:34:04 +0530

    feat(manga_reader): resolve scrolling block in MobileControlsPanel, add height adjustmnets for minimize animation

--- src/app/[mangaNamePath]/[chapter]/ControlsBox.tsx
+++ src/app/[mangaNamePath]/[chapter]/ControlsBox.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { useEffect, useMemo, useState, type FC as ReactFC } from 'react';
-import { AnimatePresence } from 'motion/react';
+import { useEffect, useMemo, useRef, useState, type FC as ReactFC } from 'react';
+import { AnimatePresence, motion } from 'motion/react';
 import { Grid2X2Plus } from 'lucide-react';
 
 import Motion from '@/components/motion';
@@ -13,6 +13,9 @@ import { cn } from '@/lib/utils';
 import useResponsive from '@/hooks/useResponsive';
 
 import { IMangaControlsBox } from './MangaReader.types';
+import MetaCardLayout from '@/widgets/MetaCardLayout';
+import Tag from '@/components/Tag';
+import MetaData from './MetaData';
 
 const ControlsBox: ReactFC<IMangaControlsBox> = ({
   mangaTitle,
@@ -24,6 +27,7 @@ const ControlsBox: ReactFC<IMangaControlsBox> = ({
   artists,
 }) => {
   const { isDesktop, isMobile, isTablet } = useResponsive();
+  const didUserMinimizeAtStart = useRef(false);
 
   const [controlsState, setControlsState] = useState({ minimize: false, isScrollTop: true });
 
@@ -33,7 +37,7 @@ const ControlsBox: ReactFC<IMangaControlsBox> = ({
   );
 
   const midScrollMaximiseAnim = useMemo(
-    () => !controlsState.isScrollTop && !controlsState.minimize,
+    () => !isDesktop && !controlsState.isScrollTop && !controlsState.minimize,
     [controlsState.isScrollTop, controlsState.minimize],
   );
 
@@ -43,81 +47,103 @@ const ControlsBox: ReactFC<IMangaControlsBox> = ({
   );
 
   const maximizeAnimVals = useMemo(
-    () => (midScrollMaximiseAnim ? { width: isTablet ? 'calc(95vw - 16px)' : '95vw' } : {}),
+    () =>
+      midScrollMaximiseAnim
+        ? { width: isTablet ? 'calc(95vw - 16px)' : '95vw', height: 'fit-content' }
+        : { width: '95vw', height: 'fit-content' },
     [midScrollMaximiseAnim],
   );
 
   useEffect(() => {
-    window.addEventListener('scroll', () => {
-      if (window.pageYOffset >= 143 && controlsState.minimize !== true) {
-        setControlsState({ minimize: true, isScrollTop: false });
-      }
+    if (!isDesktop) {
+      window.addEventListener('scroll', event => {
+        if (window.pageYOffset >= 143 && controlsState.minimize !== true) {
+          setControlsState({ minimize: true, isScrollTop: false });
+        }
 
-      if (window.pageYOffset < 50) {
-        setControlsState({ minimize: false, isScrollTop: true });
-      }
-    });
+        if (window.pageYOffset < 50) {
+          if (didUserMinimizeAtStart.current) return;
+          setControlsState({ minimize: false, isScrollTop: true });
+        }
+      });
 
-    () => window.removeEventListener('scroll', () => {});
+      () => window.removeEventListener('scroll', () => {});
+    }
   }, []);
 
   return (
-    <Motion.Card
-      animate={runAnimation ? minimizeAnimVals : maximizeAnimVals}
-      className={cn(
-        'relative ml-0 flex h-fit w-[95vw] flex-col items-start justify-between p-3 lg:px-9 lg:py-8',
-        controlsState.minimize
-          ? 'shadow-floating items-center justify-center border border-foreground p-0'
-          : '',
-        controlsState.minimize && isTablet ? 'ml-2' : '',
-        midScrollMaximiseAnim && isTablet ? 'ml-2' : '',
-        midScrollMaximiseAnim && isMobile ? 'border border-foreground' : '',
-      )}
-    >
-      <AnimatePresence>
-        {!runAnimation && (
-          <>
-            <Motion.CardHeader exit={{ opacity: 0 }} className="w-full flex-row justify-between">
-              <div className="mb-3 flex flex-col pr-2">
-                <CardTitle className="pr-6 font-heading text-2xl/7 font-medium lg:text-5xl lg:leading-[54px]">
-                  {mangaTitle}
-                </CardTitle>
-                <CardDescription className="font-heading text-xl/6 text-foreground_tint_60 lg:text-2xl/7">
-                  Chapter {currentChNum}
-                </CardDescription>
-              </div>
-              <MobileControlsPanel
-                mangaId={mangaId}
-                mangaTitle={mangaTitle}
-                totalChapters={totalCh}
-                currentChapter={Number(currentChNum)}
-                tags={tags}
-                authors={authors}
-                artists={artists}
-              />
-            </Motion.CardHeader>
-            <Motion.CardContent exit={{ opacity: 0 }} className="flex w-full gap-6">
-              <Controls
-                mangaTitle={mangaTitle}
-                currentChapter={Number(currentChNum)}
-                totalCh={totalCh}
-                mangaId={mangaId}
-                onMinimize={() => setControlsState(prev => ({ ...prev, minimize: true }))}
-              />
-            </Motion.CardContent>
-          </>
+    <>
+      <Motion.Card
+        animate={runAnimation ? minimizeAnimVals : maximizeAnimVals}
+        className={cn(
+          'relative ml-0 flex h-fit w-full flex-col items-start justify-between p-3 lg:px-9 lg:py-8',
+          runAnimation
+            ? 'shadow-floating items-center justify-center border border-foreground p-0'
+            : '',
+          controlsState.minimize && isTablet ? 'ml-2' : '',
+          midScrollMaximiseAnim && isTablet ? 'ml-2' : '',
+          midScrollMaximiseAnim && isMobile ? 'border border-foreground' : '',
         )}
-      </AnimatePresence>
+      >
+        <AnimatePresence>
+          {!runAnimation && (
+            <>
+              <Motion.CardHeader exit={{ opacity: 0 }} className="w-full flex-row justify-between">
+                <div className="mb-3 flex flex-col pr-2">
+                  <CardTitle className="pr-6 font-heading text-2xl/7 font-medium lg:text-5xl lg:leading-[54px]">
+                    {mangaTitle}
+                  </CardTitle>
+                  <CardDescription className="font-heading text-xl/6 text-foreground_tint_60 lg:text-2xl/7">
+                    Chapter {currentChNum}
+                  </CardDescription>
+                </div>
+                <MobileControlsPanel
+                  mangaId={mangaId}
+                  mangaTitle={mangaTitle}
+                  totalChapters={totalCh}
+                  currentChapter={Number(currentChNum)}
+                  tags={tags}
+                  authors={authors}
+                  artists={artists}
+                />
+              </Motion.CardHeader>
+              <Motion.CardContent exit={{ opacity: 0 }} className="flex w-full gap-6">
+                <Controls
+                  mangaTitle={mangaTitle}
+                  currentChapter={Number(currentChNum)}
+                  totalCh={totalCh}
+                  mangaId={mangaId}
+                  onMinimize={() => {
+                    if (window.scrollY < 50) {
+                      didUserMinimizeAtStart.current = true;
+                    }
+                    setControlsState(prev => ({ ...prev, minimize: true }));
+                  }}
+                />
+              </Motion.CardContent>
+            </>
+          )}
+        </AnimatePresence>
 
-      <AnimatePresence>
-        {controlsState.minimize && (
-          <Grid2X2Plus
-            size={24}
-            onClick={() => setControlsState(prev => ({ ...prev, minimize: false }))}
-          />
+        {runAnimation && (
+          <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 0.3 } }}>
+            <Grid2X2Plus
+              size={24}
+              onClick={() => setControlsState(prev => ({ ...prev, minimize: false }))}
+            />
+          </motion.div>
         )}
-      </AnimatePresence>
-    </Motion.Card>
+      </Motion.Card>
+
+      <MetaData
+        tags={tags}
+        authors={authors}
+        artists={artists}
+        className="mt-5 hidden lg:flex"
+        metaLayoutClass="lg:w-full"
+        tagClass="bg-secondary_bg2"
+      />
+    </>
   );
 };
 
