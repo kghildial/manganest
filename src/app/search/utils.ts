f7c73534e704ab70b7f4b9279b5828bc11b84341
Kinshuk Ghildial <dev.kghildial@gmail.com>
2025-05-01 20:13:23 +0530

    feat(seach): support individual tag removal

--- src/app/search/utils.ts
+++ src/app/search/utils.ts
@@ -10,12 +10,24 @@ export const filtersReducer: TFiltersReducer = (draft, action) => {
       draft.visible = false;
       break;
     case EFiltersAction.Include:
-      if (action.payload) {
+      if (action.payload && typeof action.payload === 'object') {
         const { id, name } = action.payload;
         draft.include[id] = name;
         draft.filtersChanged = true;
       } else {
-        console.error(`Payload not send in action "${EFiltersAction.Include}" in filtersReducer.`);
+        console.error(
+          `Payload not sent / incorrecy payload type in action "${EFiltersAction.Include}" in filtersReducer.`,
+        );
+      }
+      break;
+    case EFiltersAction.Exclude:
+      if (action.payload && typeof action.payload === 'string') {
+        delete draft.include[action.payload];
+        draft.filtersChanged = true;
+      } else {
+        console.error(
+          `Payload not sent / incorrecy payload type in action "${EFiltersAction.Include}" in filtersReducer.`,
+        );
       }
       break;
     case EFiltersAction.Clear:
